9c83bf91ece1a26448639c645e4288bcfe4ff12d
jarekchronowski <jarek.chronowski>
2020-01-18 11:24:45 +0100

    Dodanie klasy kontrola dostępu

--- src/pl/sda/programowanie/KontrolaDostepu.java
+++ src/pl/sda/programowanie/KontrolaDostepu.java
@@ -4,24 +4,23 @@ import java.util.Scanner;
 
 public class KontrolaDostepu {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System. int);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj rok urodzenia");
         int biezacyrok = 2020;
+        int biezacymiesiac = 1;
         int rokurodzenia = scanner.nextInt();
 
-        if (biezacyrok - rokurodzenia > 18) ;
-        {
+        if (biezacyrok - rokurodzenia > 18) {
             System.out.println("Hello");
-        }else{
-            if (biezacyrok - rokurodzenia = 18) ;
-            {
+        } else {
+            if (biezacyrok - rokurodzenia == 18) {
                 System.out.println("Podaj miesiąc w formacie 1-12");
-                int miesiac = scanner.nextInt();
-                if (biezacyMiesiac >= miesiacurodzenia);{
+                int miesiacurodzenia = scanner.nextInt();
+                if (biezacymiesiac >= miesiacurodzenia) {
                     System.out.println("Witaj weryfikacja miesiąca poprawna");
                 } else {
                     System.out.println("Brak dostępu na poziomie weryfikacji miesiąca");
-
+                }
             } else {
                 System.out.println("Brak dostępu");
 
